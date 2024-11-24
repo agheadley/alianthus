3c77c42cba7415b834d3dce38508f8cabe5c5e67
Anthony Headley <agheadley@gmail.com>
2024-11-24 13:18:37 +0000

    a2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,57 +1,36 @@
 <script lang="ts">
-	import Header from './Header.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
-<div class="app">
-	<Header />
-
-	<main>
-		{@render children()}
-	</main>
-
-	<footer>
-		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
-		</p>
-	</footer>
+	<div class="app">
+	<div class="container">
+	<header>
+		<nav>
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+			<a href="/blog">Blog</a>
+			<a href="/notes">Notes</a>
+			<a href="/guestbook">Guestbook</a>
+			<a href="/contact">Contact</a>
+		  </nav>
+		  <p>Hello!!</p>
+	  </header>
+	
+	  	<main>
+			{@render children()}
+	
+		</main>
+	
+	  <footer>
+		<p>Jane Smith's website.</p>
+	  </footer>
+	</div>
 </div>
 
+	
+
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 </style>
